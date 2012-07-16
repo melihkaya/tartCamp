0fc5f37096cbf06ce13204922710b5cb18bb2326
Melih Kaya <melihkaya88@gmail.com>
2012-07-16 17:49:38 +0300

    [tictactoe] işlemlerin yapıldığı fonksiyon oluşturuldu. 	    içeride arrayler doldurulup kıyaslama işlemine hazır hale gelmesi bekleniyor.

--- FE101/js/index.js
+++ FE101/js/index.js
@@ -24,3 +24,32 @@ $(document).ready(function(){
 		}
 });
 
+
+function processingTic(e){
+//tıklanan bölümün id si array lere eklendi.
+xArray.push(e);
+selecTicArray.push(e);
+//random üretilen sayı
+var randomNum = Math.floor(Math.random()* ticArray.length);
+
+for(var i=0;i<=selecTicArray.length;i++){
+//for içerisinde unique bir sayı üretmek istedim.	
+		
+}
+//üretilen random sayıya göre diziler oluşturuluyor.
+oArray.push(randomNum);
+selecTicArray.push(randomNum);	
+//her click de oluşan x ve o arraylerinin boyutunu aldım					
+var xNum = xArray.length;
+var oNum = oArray.length;
+							//if işlemi ile sınıra geldikten sonra ekrana image basmasını engelliyorum.
+							if(selecTicArray.length <= ticArray.length){
+							
+								$("#"+ xArray[xNum-1]).css("background","url(img/X.png)");
+								$("#"+ oArray[oNum-1]).css("background","url(img/O.png)");
+							}
+//en son oluşan dizilerin içi okunarak kazana belirlenecek.
+//yukarda takıldığım yer geçince.	
+						
+}
+
