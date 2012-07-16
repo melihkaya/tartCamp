e10f105985d8999aa9dd26ed4867370b83865709
Melih Kaya <melihkaya88@gmail.com>
2012-07-16 16:41:51 +0300

    [Array Exercise] unique alanlar arrayden çıkarılıp değerler html tablo etiketleri içerisine eklendi.

--- array.php
+++ array.php
@@ -32,3 +32,29 @@ foreach ($customerTable as $customer){
 	$customerTableKeys = array_merge($customerTableKeys, array_keys($customer));
 }
 
+//tekrar eden baslıkları ayırıp html tagları araya katılıyor
+$customerTableKeys = array_unique($customerTableKeys);
+$html .= "<tr><th>".implode("</th><th>",$customerTableKeys)."</th></tr>";
+
+foreach ($customerTable as $customer){
+
+   $html .= " <tr>";
+
+   foreach ($customerTableKeys as $key){
+
+        if (isset($customer[$key])){
+//alanlara değerleri giriyoruz
+            $html .= "<td>".$customer[$key]."</td>";
+
+        } else {
+            $html .= "<td>&nbsp;</td>";
+        }
+    }
+
+    $html .= "</tr>";
+}
+
+
+$html .= "</table>";
+//tüm değerler ekrana yazılıyor
+echo $html;
