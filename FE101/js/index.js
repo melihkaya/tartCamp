79bcdf65fb2148275f418d4cbaa828e578943e70
Melih Kaya <melihkaya88@gmail.com>
2012-07-16 17:30:42 +0300

    [tictactoe] oyunun arrayleri tanımlandı.

---
+++ FE101/js/index.js
@@ -0,0 +1,7 @@
+var selecTicArray =[]; /* seçilenlerin tutulduğu array */
+var ticArray=[]; 	   /* üretilen alan sayısı array de sıralanıyor.*/
+var xArray =[];		   /* oluşturulan x'lerin tuttulduğu array*/
+var oArray =[];		   /* oluşturulan o'lerin tuttulduğu array*/
+var randomArray = [];  /* oluşturulan o'lerin tuttulduğu array*/
+
+
