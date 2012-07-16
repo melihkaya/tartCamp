cc3c79a402e994f8b98bf93ac4fed8b2bbd8f2e6
Melih Kaya <melihkaya88@gmail.com>
2012-07-16 17:38:33 +0300

    [tictactoe] oyunun alanları ve click event oluşturuldu. tıklanan alanın id si processingTic fonksiyonuna gönderildi.

--- FE101/js/index.js
+++ FE101/js/index.js
@@ -4,4 +4,23 @@ var xArray =[];		   /* oluşturulan x'lerin tuttulduğu array*/
 var oArray =[];		   /* oluşturulan o'lerin tuttulduğu array*/
 var randomArray = [];  /* oluşturulan o'lerin tuttulduğu array*/
 
+$(document).ready(function(){
+
+		for (i=0; i<=8;i++){
+			ticArray.push(i);
+			//oyun alanalrı oluşturuluyor.
+			var myDiv = $("<div id='"+i+"' name='zone"+i+"'></div>");
+			//oluşturulan alanlar ekrana yazıldı.	
+			$("#container").append( myDiv );
+			 
+				myDiv.click(function(e){
+					var cell =$(this);
+					//alanlara tıklanıldığında id numarası işlem alanına gönderiliyor.	
+					processingTic(cell.attr("id"));
+
+				}
+			);
+		
+		}
+});
 
